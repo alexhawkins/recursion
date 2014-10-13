520a749cbe4f093b0ae15456e16d4f822a01aadd
Alex Hawkins <alexhawkins.me@gmail.com>
2014-10-13 16:53:53 -0700

    Refactor stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,17 +1,17 @@
 var stringifyJSON = function(obj) {
-    var elements = [];
     var objElements = [];
-    if (!(obj instanceof Object)) //base case, get primitive types
-        return typeof obj === 'string' ? '"' + obj + '"' : "" + obj;
-    else if (Array.isArray(obj)) // get arrays, breakdown array subcomponents via recursion
-        return '[' + (elements = obj.map(function(el) { return stringifyJSON(el); })) + ']';
-    else { //get objects
+    //check for literals
+    if (!(obj instanceof Object))
+        return typeof obj === 'string' ? '"' + obj + '"' : '' + obj;
+    //check for arrays
+    else if (Array.isArray(obj)) {
+        return '[' + obj.map(function(el) { return stringifyJSON(el); }) + ']';
+    //check for object if not array
+    } else if (obj instanceof Object) {
         for (var key in obj) {
-            if (obj[key] instanceof Function) //base case, get functions
+            if (obj[key] instanceof Function)
                 return '{}';
-            if (!(obj[key] instanceof Object)) //base case, get primitive types
-                typeof obj[key] === 'string' ? objElements.push('"' + key + '":"' + obj[key] + '"') : objElements.push('"' + key + '":' + obj[key]);
-            else //object present, break down further via recursion;
+            else
                 objElements.push('"' + key + '":' + stringifyJSON(obj[key]));
         }
         return '{' + objElements + '}';
